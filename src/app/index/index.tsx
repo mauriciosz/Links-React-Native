11164c7194efec901bc4232f86ce1f1254e9d93c
Mauricio Zaccaro <neto.mauricio@unemat.br>
2026-03-29 17:21:04 -0400

    Update no Modal para abrir com as informações do registro selecionado

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -13,9 +13,11 @@ import { categories } from "@/utils/categories";
 import { useCallback, useState } from "react";
 
 export default function Index() {
+  const [link, setLink] = useState<LinkStorage>([] as LinkStorage)
   //-- lista de LinkStorage que inicia vazia...
   const [links, setLinks] = useState<LinkStorage[]>([])
   const [category, setCategory] = useState(categories[0].name)
+  const [showModal, setShowModal] = useState(false)
 
   async function getLinks(){
     try{
@@ -32,6 +34,11 @@ export default function Index() {
     }
   }
 
+  function handleDetails(selected: LinkStorage){
+    setShowModal(true)
+    setLink(selected)
+  }
+
   //-- É executado sempre que o valor de Category mudar, chamando assim a getLinks()
   /* Essa função foi trocada pelo UseCallBack com useFocusEffect
   useEffect(() => 
@@ -43,7 +50,7 @@ export default function Index() {
     useCallback(() => {
       getLinks()  
     }, [category])
-  )
+  ) 
 
   return (
     <View style={styles.container}>
@@ -62,24 +69,24 @@ export default function Index() {
         renderItem={({item}) => (<Link 
                                     name={item.name}
                                     url={item.url}
-                                    onDetails={() => console.log("Clicou")}
+                                    onDetails={() => handleDetails(item)}
                                   />)}
         style={styles.links}                          
         contentContainerStyle={styles.linksContent}
       />
 
-      <Modal transparent visible={false}>
+      <Modal transparent visible={showModal} animationType="slide">
         <View style={styles.modal}>
           <View style={styles.modalContent}>
             <View style={styles.modalHeader}>
-              <Text style={styles.modalCategory}>Curso</Text>
-              <TouchableOpacity>
+              <Text style={styles.modalCategory}>{link.category}</Text>
+              <TouchableOpacity onPress={() => setShowModal(false)}>
                 <MaterialIcons name="close" size={20} color={colors.gray[400]}/>
               </TouchableOpacity>
             </View>            
 
-            <Text style={styles.modalLinkName}>Rocketseat</Text>
-            <Text style={styles.modalUrl}>https://www.rocketseat.com.br/</Text>
+            <Text style={styles.modalLinkName}>{link.name}</Text>
+            <Text style={styles.modalUrl}>{link.url}</Text>
 
             <View style={styles.modalFooter}>
               <Option name="Excluir" icon="delete" variant="secundary"/>
